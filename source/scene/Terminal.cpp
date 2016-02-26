0697d2576202da3a4ab18ae89cc1878e0fb558f0
Dorian Wouters <gravgun@openmailbox.org>
2016-02-26 17:34:13 +0100

    Delete Screen and Terminal
    To be replaced later with a more generic visual/interaction stack.

--- source/scene/Terminal.cpp
+++
@@ -1,9 +0,0 @@
-#include <radix/scene/Terminal.hpp>
-
-namespace radix {
-
-Terminal::Terminal():
-  enabled(false),
-  textColor(0, 0, 0, 1), backgroundColor(1, 1, 1, 1) {
-}
-} /* namespace radix */
