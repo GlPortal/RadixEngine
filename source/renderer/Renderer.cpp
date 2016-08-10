c4fa2dd0923e43dbd1b63402fe3935c52b60250e
Henry Hirsch <henry@w3-net.de>
2016-08-10 11:41:58 +0200

    Changed comment block to doxygen comment block in renderer

--- source/renderer/Renderer.cpp
+++ source/renderer/Renderer.cpp
@@ -39,10 +39,12 @@
 
 namespace radix {
 
-/*
+/**
  * This is the low level graphics renderer
  * it is highly encouraged to move all
  * specialized code into sub-renderers.
+ *
+ * @param World The world to render
  */
 Renderer::Renderer(World &w) :
   lightsUBO(0),
