e39df773e3078b7cfaf3f70dde034cf6d69f42fe
Geert Custers <geert.aj.custers@gmail.com>
2016-09-27 22:41:32 +0200

    Fix compile errors

--- include/radix/core/state/HandleGameFunction.hpp
+++ include/radix/core/state/HandleGameFunction.hpp
@@ -5,10 +5,12 @@
 #ifndef HANDLEGAMEFUNCTION_HPP
 #define HANDLEGAMEFUNCTION_HPP
 
+namespace radix {
+
 class BaseGame;
 
-namespace radix {
 typedef std::function<void (BaseGame &game)> HandleGameFunction;
+
 } /* namespace radix */
 
 #endif //GLPORTAL_HANDLEGAMEFUNCTION_HPP
