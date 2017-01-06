9282271a0eb24f734a6f2ea4a0afd7db9399987d
Henry Hirsch <henry@w3-net.de>
2017-01-06 20:14:38 +0100

    Add enums for elements align and positioning mode

--- include/radix/data/screen/Element.hpp
+++ include/radix/data/screen/Element.hpp
@@ -6,17 +6,25 @@
 #include <radix/core/math/Vector4f.hpp>
 
 namespace radix {
+enum PositionMode {
+  absolute,
+  relative
+};
 
-class Element {
-public:
-  static const std::string POSITION_ABSOLUTE;
-  static const std::string POSITION_RELATIVE;
+enum AlignMode {
+  left,
+  right,
+  center
+};
+
+struct Element {
   std::vector<Element> elements;
   Vector4f color;
   unsigned int zIndex;
   Vector4f position;
   Vector4f margin;
   Vector4f padding;
+
 };
 } /* namespace radix */
 
