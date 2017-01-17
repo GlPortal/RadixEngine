e0afc784bbaef1e810d70cba4f882e9d0cc36930
Henry Hirsch <henry@w3-net.de>
2017-01-17 23:00:43 +0100

    Add setters and getters to style

--- include/radix/data/screen/Style.hpp
+++ include/radix/data/screen/Style.hpp
@@ -1,6 +1,9 @@
 #ifndef RADIX_STYLE_HPP
 #define RADIX_STYLE_HPP
 
+#include <map>
+#include <utility>
+
 #include <radix/core/math/Vector4f.hpp>
 #include <radix/core/math/Vector4i.hpp>
 
@@ -19,14 +22,44 @@ namespace radix {
   /**
    * Represents element style
    */
-  struct Style {
-    Vector4f color;
-    Vector4i position;
-    Vector4i margin;
-    Vector4i padding;
-    AlignMode alignMode;
-    PositionMode positionMode;
+  class Style {
+  public:
+    bool hasColor();
+    void setColor(Vector4i color);
+    Vector4i getColor();
+    bool hasPosition();
+    void setPosition(Vector4i position);
+    Vector4i getPosition();
+    bool hasMargin();
+    void setMargin(Vector4i margin);
+    Vector4i getMargin();
+    bool hasPadding();
+    void setPadding(Vector4i padding);
+    Vector4i getPadding();
+    bool hasAlignMode();
+    AlignMode getAlignMode();
+    void setAlignMode(AlignMode mode);
+    bool hasPositionMode();
+    PositionMode getPositionMode();
+    void setPositionMode(PositionMode mode);
+  private:
+    std::map<std::string, Vector4i> vector4iMap;
+    std::pair <Vector4i, bool> color;
+    std::pair <Vector4i, bool> position;
+    std::pair <Vector4i, bool> margin;
+    std::pair <Vector4i, bool> padding;
+    std::pair <AlignMode, bool> alignMode;
+    std::pair <PositionMode, bool> positionMode;
   };
+
+  inline Style operator+(Style& leftStyle, Style& rightStyle)
+  {
+    Style style = leftStyle;
+    if (rightStyle.hasColor() && !leftStyle.hasColor()) {
+      style.setColor(rightStyle.getColor());
+    }
+    return style;
+  }
 } /* namespace radix */
 
 #endif /* RADIX_STYLE_HPP */
---
+++ source/data/screen/Style.cpp
@@ -0,0 +1,38 @@
+#include <radix/data/screen/Style.hpp>
+
+namespace radix {
+  bool Style::hasColor() {
+    return color.second;
+  }
+
+  void Style::setColor(Vector4i color) {
+
+  }
+
+  Vector4i Style::getColor() {}
+  bool Style::hasPosition() {
+    return position.second;
+  }
+  void Style::setPosition(Vector4i position) {}
+  Vector4i Style::getPosition() {}
+  bool Style::hasMargin() {
+    return margin.second;
+  }
+  void Style::setMargin(Vector4i margin) {}
+  Vector4i Style::getMargin() {}
+  bool Style::hasPadding() {
+    return padding.second;
+  }
+  void Style::setPadding(Vector4i padding) {}
+  Vector4i Style::getPadding() {}
+  bool Style::hasAlignMode() {
+    return alignMode.second;
+  }
+  AlignMode Style::getAlignMode() {}
+  void Style::setAlignMode(AlignMode mode) {}
+  bool Style::hasPositionMode() {
+    return positionMode.second;
+  }
+  PositionMode Style::getPositionMode() {}
+  void Style::setPositionMode(PositionMode mode) {}
+} /* namespace radix */
