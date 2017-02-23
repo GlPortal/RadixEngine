ddcfdf56e5fa109242d427fde6ea75941600cf1b
acterhd <acterhd@gmail.com>
2017-02-24 01:25:44 +0800

    less conflicts

--- source/Window.cpp
+++ source/Window.cpp
@@ -40,16 +40,7 @@ void Window::setConfig(radix::Config &config){
 }
 
 void Window::initOpenGL() {
-  #ifdef EPOXY_SUPPORT
-    const int glver = epoxy_gl_version(), glmaj = glver / 10, glmin = glver % 10;
-    const std::string versionString = std::to_string(glmaj) + '.' + std::to_string(glmin);
-    Util::Log(Verbose, "Window") << "OpenGL " << versionString;
-
-    if (glver < 32) {
-        throw Exception::Error("Window", std::string("OpenGL Version ") + versionString +
-            " is unsupported, " "required minimum is 3.2");
-    }
-#elif GLAD_SUPPORT
+#ifdef GLAD_SUPPORT
     if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
         Util::Log(Error, "Window") << "Failed to initialize OpenGL context";
         const std::string versionString = std::to_string(GLVersion.major) + '.' + std::to_string(GLVersion.minor);
@@ -58,6 +49,19 @@ void Window::initOpenGL() {
     }
 
     Util::Log(Info, "Window") << "Loaded OpenGL version: " << GLVersion.major << "." << GLVersion.minor;
+#elif EPOXY_SUPPORT
+    const int glver = epoxy_gl_version(), glmaj = glver / 10, glmin = glver % 10;
+    const std::string versionString = std::to_string(glmaj) + '.' + std::to_string(glmin);
+    Util::Log(Verbose, "Window") << "OpenGL " << versionString;
+    if (config.getIgnoreGlVersion()) {
+      Util::Log(Warning, "Window") << "Ignore OpenGl version";
+    }
+    else {
+      if (glver < 32) {
+        throw Exception::Error("Window", std::string("OpenGL Version ") + versionString +
+              " is unsupported, " "required minimum is 3.2");
+      }
+    }
 #endif
 }
 
