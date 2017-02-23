2bc7aa41d6f58612859cde1bab7a748c0012ca85
Julian Thijssen <Nimthora@gmail.com>
2017-02-23 15:56:59 +0100

    Remove support check for uniform buffers since they are always available since OpenGL 3.1

--- source/renderer/Renderer.cpp
+++ source/renderer/Renderer.cpp
@@ -22,7 +22,6 @@ Renderer::Renderer(World &world) :
   rc(*this),
   textRenderer(world, *this),
   vpWidth(0), vpHeight(0) {
-  support.uniformBuffers = epoxy_has_gl_extension("GL_ARB_uniform_buffer_object");
 }
 
 void Renderer::init() {
