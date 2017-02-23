478a060a740dce2a6e1e17bb8f6f7e00f58236e4
acterhd <acterhd@gmail.com>
2017-02-24 01:27:52 +0800

    less layers

--- source/Window.cpp
+++ source/Window.cpp
@@ -41,27 +41,27 @@ void Window::setConfig(radix::Config &config){
 
 void Window::initOpenGL() {
 #ifdef GLAD_SUPPORT
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
-        Util::Log(Error, "Window") << "Failed to initialize OpenGL context";
-        const std::string versionString = std::to_string(GLVersion.major) + '.' + std::to_string(GLVersion.minor);
-        throw Exception::Error("Window", std::string("OpenGL Version ") + versionString +
+  if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    Util::Log(Error, "Window") << "Failed to initialize OpenGL context";
+    const std::string versionString = std::to_string(GLVersion.major) + '.' + std::to_string(GLVersion.minor);
+    throw Exception::Error("Window", std::string("OpenGL Version ") + versionString +
+          " is unsupported, " "required minimum is 3.2");
+  }
+
+  Util::Log(Info, "Window") << "Loaded OpenGL version: " << GLVersion.major << "." << GLVersion.minor;
+#elif EPOXY_SUPPORT
+  const int glver = epoxy_gl_version(), glmaj = glver / 10, glmin = glver % 10;
+  const std::string versionString = std::to_string(glmaj) + '.' + std::to_string(glmin);
+  Util::Log(Verbose, "Window") << "OpenGL " << versionString;
+  if (config.getIgnoreGlVersion()) {
+    Util::Log(Warning, "Window") << "Ignore OpenGl version";
+  }
+  else {
+    if (glver < 32) {
+      throw Exception::Error("Window", std::string("OpenGL Version ") + versionString +
             " is unsupported, " "required minimum is 3.2");
     }
-
-    Util::Log(Info, "Window") << "Loaded OpenGL version: " << GLVersion.major << "." << GLVersion.minor;
-#elif EPOXY_SUPPORT
-    const int glver = epoxy_gl_version(), glmaj = glver / 10, glmin = glver % 10;
-    const std::string versionString = std::to_string(glmaj) + '.' + std::to_string(glmin);
-    Util::Log(Verbose, "Window") << "OpenGL " << versionString;
-    if (config.getIgnoreGlVersion()) {
-      Util::Log(Warning, "Window") << "Ignore OpenGl version";
-    }
-    else {
-      if (glver < 32) {
-        throw Exception::Error("Window", std::string("OpenGL Version ") + versionString +
-              " is unsupported, " "required minimum is 3.2");
-      }
-    }
+  }
 #endif
 }
 
