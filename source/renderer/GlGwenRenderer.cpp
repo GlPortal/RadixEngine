235c51753438ce44d21f3efa6835a1ee47b25917
Henry Hirsch <henry@w3-net.de>
2017-02-24 19:18:07 +0100

    Remove commented out line in GlGwenRenderer.cpp

--- source/renderer/GlGwenRenderer.cpp
+++ source/renderer/GlGwenRenderer.cpp
@@ -55,7 +55,6 @@ void GlGwenRenderer::Begin() {
   glDisable(GL_DEPTH_TEST);
   glDisable(GL_CULL_FACE);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  //glAlphaFunc(GL_GREATER, 1.0f);
   glEnable(GL_BLEND);
 }
 
