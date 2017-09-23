905c2ff55e97233cce79817f7179bfc2d7696f44
codercodi <codercodi@gmail.com>
2017-09-23 17:18:50 -0400

    made changes to handle file format from .gmd to .rmdx

--- source/data/material/MaterialLoader.cpp
+++ source/data/material/MaterialLoader.cpp
@@ -15,7 +15,7 @@ std::map<std::string, Material> MaterialLoader::materialCache = {};
 const Material MaterialLoader::loadFromXML(const std::string &path) {
   std::string dir = path.substr(0, path.find_last_of("/\\"));
   XMLDocument doc;
-  XMLError error = doc.LoadFile((Environment::getDataDir() + "/textures/" + path + ".gmd").c_str());
+  XMLError error = doc.LoadFile((Environment::getDataDir() + "/textures/" + path + ".rmdx").c_str());
 
   if (error != 0) {
     Util::Log(Error, "MaterialLoader") << "XML Error " << doc.ErrorID() << ": " <<
