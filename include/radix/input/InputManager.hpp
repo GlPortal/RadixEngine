890dcf1b415df24b3cf46b7ee85bf43f323de532
babyccino <gus.ryan163@gmail.com>
2017-11-05 15:58:47 +0800

    Update InputManager.hpp

--- include/radix/input/InputManager.hpp
+++ include/radix/input/InputManager.hpp
@@ -13,21 +13,21 @@ class InputSource;
 class InputManager {
 public:
 	enum Action : int8_t {
-	  ACTION_INVALID = -1,
-	  PLAYER_MOVE_X = 0,
-	  PLAYER_MOVE_Y = 1,
-	  PLAYER_LOOK_X = 2,
-	  PLAYER_LOOK_Y = 3,
-	  PLAYER_JUMP = 4,
-	  PLAYER_PORTAL_0 = 5,
-	  PLAYER_PORTAL_1 = 6,
-	PLAYER_FORWARD = 7,
+		ACTION_INVALID = -1;
+		PLAYER_MOVE_X = 0,
+		PLAYER_MOVE_Y = 1,
+		PLAYER_LOOK_X = 2,
+		PLAYER_LOOK_Y = 3,
+		PLAYER_JUMP = 4,
+		PLAYER_PORTAL_0 = 5,
+		PLAYER_PORTAL_1 = 6,
+		PLAYER_FORWARD = 7,
 		PLAYER_BACK = 8,
 		PLAYER_LEFT = 9,
 		PLAYER_RIGHT = 10,
 		GAME_PAUSE = 11,
 		GAME_QUIT = 12,
-	  ACTION_MAX = 13
+		ACTION_MAX = 13
 	};
 
 	using StateArray = std::array<float, ACTION_MAX>;
